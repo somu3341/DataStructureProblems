0766cf7177c4ae8bc4603e00afd594e97b95a352
somu3341 <somashekarvsv1996@gmail.com>
2023-03-22 12:04:20 +0530

    [somu]Add:Adding in Reverse order and Displaying.

--- DataStructureProblems/Program.cs
+++ DataStructureProblems/Program.cs
@@ -9,6 +9,11 @@ namespace DataStructureProblems
             linkedList.Add(56);
             linkedList.Add(30);
             linkedList.Add(70);
+            //linkedList.Display();
+            linkedList.AddInReverseOrder(70);
+            linkedList.AddInReverseOrder(30);
+            linkedList.AddInReverseOrder(56);
+            linkedList.Display();
         }
     }
 }
