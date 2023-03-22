26a817a8c653786689c40bc83bf66d1ca645630c
somu3341 <somashekarvsv1996@gmail.com>
2023-03-22 11:31:57 +0530

    [somu]Add:Adding Data.

---
+++ DataStructureProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DataStructureProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
